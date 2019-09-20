35d60f1885d95bbc6e830baaee9b163251e30eee
Austin Clyde <aclyde@uchicago.edu>
2019-09-20 08:26:15 -0500

    Initial commit

---
+++ model.py
@@ -0,0 +1,28 @@
+from keras.layers import Input, Dense, Dropout
+from keras.models import Model
+
+def get_model(input_size=784):
+    inputs = Input(shape=(input_size,))
+
+    # a layer instance is callable on a tensor, and returns a tensor
+    x = Dense(512, activation='relu')(inputs)
+    x = Dropout(0.45)(x)
+    x = Dense(512, activation='relu')(x)
+    x = Dropout(0.45 * 0.8)(x)
+    x = Dense(256, activation='relu')(x)
+    x = Dropout(0.45 * 0.7)(x)
+    x = Dense(128, activation='relu')(x)
+    x = Dropout(0.45 * 0.6)(x)
+    x = Dense(128, activation='relu')(x)
+    x = Dropout(0.45 * 0.4)(x)
+    x = Dense(64, activation='relu')(x)
+    x = Dropout(0.45 * 0.3)(x)
+    x = Dense(64, activation='relu')(x)
+    x = Dropout(0.45 * 0.2)(x)
+
+    predictions = Dense(1, activation=None)(x)
+
+    # This creates a model that includes
+    # the Input layer and three Dense layers
+    model = Model(inputs=inputs, outputs=predictions)
+    return get_model
---
+++ train_model.py
@@ -0,0 +1,25 @@
+from model import get_model
+import numpy as np
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--trainX', help='trainX npy', type=str, required=True)
+    parser.add_argument('--trainY', help='trainY npy', type=str, required=True)
+    parser.add_argument('--testX', help='testX npy', type=str, required=True)
+    parser.add_argument('--testY', help='testY npy', type=str, required=True)
+    args = parser.parse_args(())
+    return args
+
+args = get_args()
+
+trainX, trainY = np.load(args.trainX), np.load(args.trainY)
+testX, testY = np.load(args.testX), np.load(args.testY)
+
+
+model = get_model(trainX.shape[1])
+
+model.compile(optimizer='adam',
+              loss='mse',
+              metrics=['mse'])
+model.fit(trainX, trainY, epochs=50, batch_size=32 )
